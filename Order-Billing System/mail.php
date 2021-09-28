79f100fa20a0dc5445f9fe1f56db16fe46193f10
Pranav Satav <37726730+PranavSatav@users.noreply.github.com>
2021-09-28 14:17:45 +0530

    Update mail.php

--- Order-Billing System/mail.php
+++ Order-Billing System/mail.php
@@ -11,7 +11,7 @@ $message = $_POST['message'];
 $formcontent=" Full Name: $name \n Contact No: $phone  \n Customer Email: $email  \n Note to Seller: $website \n Country: $priority \n State: $type \n Mode of Payment: $payment \n Shipping Address: $message";
 $recipient = "pranvsatv@gmail.com , $email";
 $subject = "Order Placed!";
-$mailheader = "From: Ikshana <ikshana@gmail.com> \r\n";
+$mailheader = "From: Pranav <pranav@gmail.com> \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 header("Location: https://nubz.000webhostapp.com/restaurant/thankyou.html"); 
 ?>
